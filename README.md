2bdc105b7d2497961e370e931a4ecdfa8b368659
Māris Muižnieks <maris.muiznieks@lumi.moe>
2024-12-08 12:33:15 +0200

    d1 p2 solved

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Solutions for Advent of Code 2024 in Zig
 
 Learning Zig or something I guess. Just for fun.
+
+## How to run:
+Simply go into the day's folder and `zig run dX.zig`. For now that's how I'm doing it, but I might refactor everything into a single executable once I get a better understanding of Zig.
+
+## Day 1 
+Simple problem, but doing it in a language you haven't done anything in is a bit wonky and slow. Still fun.
+### Problem 1
+Day 1 Problem 1 has been solved, relativelly simple.
+### Problem 2 
+Day 1 Problem 2 has been solved, it was easier in my opinion :^)
--- day1/d1.zig
+++ day1/d1.zig
@@ -41,13 +41,58 @@ pub fn solve1(content: []const u8) !i64 {
     return sum;
 }
 
-//pub fn solve2(content: []const u8) !i64 {
-//    return 1;
-//}
+pub fn solve2(content: []const u8) !i64 {
+    // TODO: Same for this, i really should fix it up, and get the actual content to be parsed properly.
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    defer _ = gpa.deinit();
+    var lines = std.mem.tokenize(u8, content, "\n");
+    var left = std.ArrayList(i64).init(alloc);
+    defer left.deinit();
+    var right = std.ArrayList(i64).init(alloc);
+    defer right.deinit();
+
+    while (lines.next()) |line| {
+        var nums = std.mem.tokenize(u8, line, " ");
+        if (nums.next()) |num1| {
+            if (std.fmt.parseInt(i64, num1, 10)) |parsed1| {
+                try left.append(parsed1);
+            } else |_| {
+                std.debug.print("Error parsing nums: {s}\n", .{num1});
+                continue;
+            }
+        }
+        if (nums.next()) |num2| {
+            const trimmed = std.mem.trim(u8, num2, &std.ascii.whitespace);
+            if (std.fmt.parseInt(i64, trimmed, 10)) |parsed2| {
+                try right.append(parsed2);
+            } else |_| {
+                std.debug.print("Error parsing nums: {s}\n", .{trimmed});
+                continue;
+            }
+        }
+    }
+    // ^^ all the above is kind of useless, but for now it stays untill I read up on how to do this properly.
+
+    // Welp that wasn't supper hard :^)
+    var sum: i64 = 0;
+    for (left.items) |l| {
+        var tmp: i64 = 0;
+        for (right.items) |r| {
+            if (l == r) {
+                tmp += 1;
+            }
+        }
+        sum += l * tmp;
+        tmp = 0;
+    }
+
+    return sum;
+}
 
 pub fn main() !void {
     const data = @embedFile("d1.txt");
     const ans1 = try solve1(data);
-    //   const ans2 = try solve2(data);
-    std.debug.print("Answer 1: {}\n", .{ans1});
+    const ans2 = try solve2(data);
+    std.debug.print("Answer 1: {}\nAnswer 2: {}\n", .{ ans1, ans2 });
 }
