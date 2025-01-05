ff5066da7e739b032753c459b09be9a4c9b76f1c
Māris Muižnieks <maris.muiznieks@lumi.moe>
2025-01-05 12:35:36 +0200

    \_:)_/

--- day2/d2.zig
+++ day2/d2.zig
@@ -1,7 +1,22 @@
 const std = @import("std");
 
-pub fn solve1() i32 {
-    return 1;
+pub fn solve1(content: []const u8) !i64 {
+    var lines = std.mem.tokenize(u8, content, "\n");
+    var ret: i64 = 0;
+    while (lines.next()) |line| {
+        var nums = std.mem.tokenize(u8, line, " ");
+        while (nums.next()) |number| {
+            const trimmed = std.mem.trim(u8, number, &std.ascii.whitespace);
+            ret += try std.fmt.parseInt(i64, trimmed, 10);
+            //if (std.fmt.parseInt(i64, trimmed, 10)) |parsed| {
+            //    ret += try parsed;
+            //} else {
+            //    std.debug.print("Error, parsing nums: {s}\n", .{number});
+            //    continue;
+            //}
+        }
+    }
+    return ret;
 }
 
 pub fn solve2() i32 {
@@ -9,7 +24,8 @@ pub fn solve2() i32 {
 }
 
 pub fn main() void {
-    const ans1 = solve1();
-    const ans2 = solve2();
+    const data = @embedFile("d2.txt");
+    const ans1 = try solve1(data);
+    const ans2 = try solve2();
     std.debug.print("Answer 1: {}\nAnswer 2: {}\n", .{ ans1, ans2 });
 }
