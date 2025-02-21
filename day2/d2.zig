1a846e81293fa2b89dd1abd2d061203b3d954aec
Māris Muižnieks <maris.muiznieks@lumi.moe>
2025-02-21 23:32:02 +0200

    Update d2.zig

--- day2/d2.zig
+++ day2/d2.zig
@@ -3,29 +3,18 @@ const std = @import("std");
 pub fn solve1(content: []const u8) !i64 {
     var lines = std.mem.tokenize(u8, content, "\n");
     var ret: i64 = 0;
+    // ? wacky shit doesn't work I guess ):
     while (lines.next()) |line| {
-        var nums = std.mem.tokenize(u8, line, " ");
-        while (nums.next()) |number| {
-            const trimmed = std.mem.trim(u8, number, &std.ascii.whitespace);
-            ret += try std.fmt.parseInt(i64, trimmed, 10);
-            //if (std.fmt.parseInt(i64, trimmed, 10)) |parsed| {
-            //    ret += try parsed;
-            //} else {
-            //    std.debug.print("Error, parsing nums: {s}\n", .{number});
-            //    continue;
-            //}
-        }
+        //var nums = std.mem.tokenize(u8, line, " ");
+        std.debug.print("line {}\n", .{std.mem.tokenize(u8, line, " ")});
     }
+    ret = 1;
     return ret;
-}
-
-pub fn solve2() i32 {
-    return 2;
 }
 
 pub fn main() void {
     const data = @embedFile("d2.txt");
     const ans1 = try solve1(data);
-    const ans2 = try solve2();
-    std.debug.print("Answer 1: {}\nAnswer 2: {}\n", .{ ans1, ans2 });
+    //const ans2 = try solve2();
+    std.debug.print("Answer 1: {}\nAnswer 2: {}\n", .{ans1});
 }
